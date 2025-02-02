7ff2d1e570495c14fc31d01f53fede8938e0e0c9
dhirajs16 <sarrafd16@gmail.com>
2025-02-02 14:43:01 +0545

    Navbar and hero section

--- frontend_clone/iphone/src/components/Hero.jsx
+++ frontend_clone/iphone/src/components/Hero.jsx
@@ -1,5 +1,53 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useEffect, useState } from "react";
+import { heroVideo, smallHeroVideo } from "../utils";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleResize = () => {
+    window.innerWidth < 760
+      ? setVideoSrc(smallHeroVideo)
+      : setVideoSrc(heroVideo);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to("#hero-title", { opacity: 1, delay: 1.5 });
+    gsap.to("#cta", {y: -100, delay: 2.5, opacity: 1})
+  }, []);
+
+  return (
+    <section className="w-screen nav-height relative">
+      {/* Hero title and video */}
+      <div className="w-full h-5/6 flex-center flex-col">
+        <p id="hero-title" className="text-sm hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="w-9/12 md:w-10/12">
+          <video autoPlay key={videoSrc} playsInline={true} muted className="pointer-events-none outline-none">
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+
+      {/* buy button */}
+      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+        <a href="" className="btn">Buy</a>
+        <p className="text-xl font-normal">From $199/month or $999</p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- frontend_clone/iphone/src/components/Navbar.jsx
+++ frontend_clone/iphone/src/components/Navbar.jsx
@@ -1,20 +1,30 @@
+import { navLists } from "../constants";
 import { appleImg, bagImg, searchImg } from "../utils";
 
 const Navbar = () => {
   return (
-    <header>
-      <nav>
+    <header className="w-screen p-5 sm:px-10 ">
+      <nav className="w-full screen-max-width flex justify-between items-center">
         {/* Apple logo */}
         <img src={appleImg} width={18} height={18} alt="apple" />
 
-        {/* Nav Items */}
-        {["Phones", "Macbooks", "Tablets"].map((item) => (
-          <div key={item}>{item}</div>
-        ))}
+        {/* Nav List Items */}
+        <div className="max-sm:hidden flex gap-5">
+          {navLists.map((item) => (
+            <div
+              key={item}
+              className="text-gray cursor-pointer hover:text-white transition-all"
+            >
+              {item}
+            </div>
+          ))}
+        </div>
 
         {/* Search and Bag Image */}
-        <img src={searchImg} width={18} height={18} alt="search" />
-        <img src={bagImg} width={18} height={18} alt="bag" />
+        <div className="flex sm:gap-7 gap-5 items-center">
+          <img src={searchImg} width={18} height={18} alt="search" />
+          <img src={bagImg} width={18} height={18} alt="bag" />
+        </div>
       </nav>
     </header>
   );
